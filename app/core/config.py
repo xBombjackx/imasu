386402b71cd81948394ffe6157621ab53e726cec
MJ <mikehaggar@gmail.com>
2025-09-15 21:32:00 -0400

    docker added

--- app/core/config.py
+++ app/core/config.py
@@ -1,5 +1,8 @@
-# URL for the AUTOMATIC1111 API
-A1111_URL = "http://127.0.0.1:7860"
+import os
 
-# The output directory for generated images
-OUTPUT_DIR = "final_outputs"
+# The URL for the AUTOMATIC1111 API
+# It will use the environment variable from Docker Compose, or default to localhost
+A1111_URL = os.getenv("A1111_URL", "http://localhost:7860")
+
+# The directory to save generated images
+OUTPUT_DIR = "output"
